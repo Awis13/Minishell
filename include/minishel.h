a415b7c974a2d23d77b97b6021812e4d73f6324e
Nipostni <awis@me.com>
2023-07-02 14:20:09 +0200

    Convert include directory to a regular directory

---
+++ include/minishel.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "get_next_line.h"
+#include "ft_printf.h"
+#include "libft.h"
+
+void cmd_exit(char **args);
+void cmd_pwd(char **args);
+void cmd_echo(char **args);
